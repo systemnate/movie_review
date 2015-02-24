e460240bb2191abef3891b6dd7726419f25eee8f
Nate Dalo <nate.dalo@yahoo.com>
2015-02-24 13:22:52 -0600

    Change Paperclip path for production to /usr/bin/convert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
-  Paperclip.options[:command_path] = "/usr/bin/"  
+  Paperclip.options[:command_path] = "/usr/bin/convert"  
 end
