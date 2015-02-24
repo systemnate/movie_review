b0ed21c43f8e433b57e8c61072d2333cd57e217e
Nate Dalo <nate.dalo@yahoo.com>
2015-02-24 13:45:53 -0600

    Add url to has_attached_file method in Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, styles: { medium: "400x600#", small: "175x300#" }
+  has_attached_file :image, styles: { medium: "400x600#", small: "175x300#" }, url: ":s3_domain_url"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
