ebc5d03f67e8591d7568e9038dce93e398732869
Nate Dalo <nate.dalo@yahoo.com>
2015-02-24 13:20:30 -0600

    Add Paperclip command_path to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,6 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-  }  
+  }
+  Paperclip.options[:command_path] = "/usr/bin/"  
 end
