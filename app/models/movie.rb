1815cefd631ebd241eb881d35ead27675ac1ddfe
Nate Dalo <nate.dalo@yahoo.com>
2015-02-26 12:39:54 -0600

    Changed minimum length to 3 for movie title and changed wording

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,7 +6,7 @@ class Movie < ActiveRecord::Base
   has_many :reviews
   has_attached_file :image, styles: { medium: "400x600#", small: "175x300#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  validates :title,  presence: true, length: { minimum: 5, maximum: 75 }, uniqueness: { case_sensitive: false }
+  validates :title,  presence: true, length: { minimum: 3, maximum: 75 }, uniqueness: { case_sensitive: false }
   validates :description,  presence: true, length: { minimum: 5, maximum: 500 }
 
   def image_from_url(url)
