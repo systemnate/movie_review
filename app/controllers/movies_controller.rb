b91c489bc7c068134278e4620f3601c626941666
Nate Dalo <nate.dalo@yahoo.com>
2015-02-27 15:50:14 -0600

    Add Google Analytics

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,8 @@ class MoviesController < ApplicationController
   end
 
   def show
+    #Future enhancement to show similar movies:
+    #@similar = Tmdb::Movie.similar_movies(@movie.id)
     @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
     if @reviews.blank?
       @avg_review = 0
