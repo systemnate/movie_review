982abbeef2ae57ecf3f034822af14ec83af2fa22
Nate Dalo <nate.dalo@yahoo.com>
2015-03-01 10:51:18 -0600

    Removing round until further research

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,7 +26,7 @@ class MoviesController < ApplicationController
     if @reviews.blank?
       @avg_review = 0
     else
-      @avg_review = @reviews.average(:rating).round(2)
+      @avg_review = @reviews.average(:rating)
     end
   end
 
