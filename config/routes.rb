8d69a1571919537f3904a302e8cccdad4d3ceabc
Nate Dalo <nate.dalo@yahoo.com>
2015-03-02 16:00:42 -0600

    Add sitemap to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   get '/about', to: 'pages_controller#about'
+  get '/sitemap.xml', to: 'pages_controller#sitemap'
   get '/google8cbf67f657da8a59.html', to: 'pages_controller#google'
   devise_for :users
   
