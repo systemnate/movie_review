91fbaa18a625b810891f7baab00203bfea7e871c
Nate Dalo <nate.dalo@yahoo.com>
2015-03-02 15:59:41 -0600

    Add sitemap.xml

--- app/controllers/pages_controller_controller.rb
+++ app/controllers/pages_controller_controller.rb
@@ -1,7 +1,12 @@
 class PagesControllerController < ApplicationController
   def about
   end
+  
   def google
+    render :layout => false
+  end
+
+  def sitemap
     render :layout => false
   end
 end
