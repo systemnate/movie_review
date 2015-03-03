8e811b74d0722b4471ff735127456fb551222cad
Nate Dalo <nate.dalo@yahoo.com>
2015-03-02 18:03:57 -0600

    Change maximum description to 1000 characters

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "400x600#", small: "175x300#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :title,  presence: true, length: { minimum: 3, maximum: 75 }, uniqueness: { case_sensitive: false }
-  validates :description,  presence: true, length: { minimum: 5, maximum: 500 }
+  validates :description,  presence: true, length: { minimum: 5, maximum: 1000 }
   def image_from_url(url)
     self.image = open(URI.parse(url))
   rescue
