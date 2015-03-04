470471aa161df99189aea430b357610b3db6e2b2
Nate Dalo <nate.dalo@yahoo.com>
2015-03-04 10:18:28 -0600

    Add name field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304154320) do
+ActiveRecord::Schema.define(version: 20150304161805) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20150304154320) do
     t.boolean  "admin"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
