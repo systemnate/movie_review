045c905b7fea57113f8117ff49e3e04e76396b0b
Nate Dalo <nate.dalo@yahoo.com>
2015-03-04 10:32:15 -0600

    Remove index from Users>email

---
+++ db/migrate/20150304163149_remove_index.rb
@@ -0,0 +1,4 @@
+class RemoveIndex < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304162547) do
+ActiveRecord::Schema.define(version: 20150304163149) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
