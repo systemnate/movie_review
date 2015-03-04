c369daefc40d82726893192135a054c764857aa4
Nate Dalo <nate.dalo@yahoo.com>
2015-03-03 19:31:44 -0600

    Add API for showing single movie

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -2,4 +2,9 @@ class Api::MoviesController < ApplicationController
   def index
     render json: Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+    render json: @movie
+  end
 end
