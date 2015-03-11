0f274c7558fb73e78de34dd8855129adb62f3356
Nate Dalo <nate.dalo@yahoo.com>
2015-03-11 15:52:12 -0500

    Allow viewing of fresh movies without signing in

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :fresh]
 
   attr_accessor :image_file_name
 
