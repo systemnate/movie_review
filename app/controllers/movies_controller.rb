7fcb7fc7ce51447f845cfa66f3d6f561bb98987d
Nate Dalo <nate.dalo@yahoo.com>
2015-04-10 14:26:20 -0500

    Add caching to index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  caches_page :index
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show, :fresh]
 
